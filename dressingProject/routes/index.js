7e5758c06e5c7a9c60b2af48fc277bf22a3a9799
paulbourceret <paulbourceret@gmail.com>
2019-07-01 15:52:22 +0200

    user models

--- dressingProject/routes/index.js
+++ dressingProject/routes/index.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const app = express();
+const User = require("../models/users");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 /* GET sign in page */
 router.get("/", (req, res, next) => {
   res.render("login");
 });
 
-router.post("/", (req, res, next) => {});
+router.post("/", (req, res, next) => {
+  const name = req.body.name;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.pass;
+  if (
+    name === "" ||
+    lastname === "" ||
+    email === "" ||
+    username === "" ||
+    password === ""
+  ) {
+    res.render("/", { message: "fill out all the form to sign up" });
+  }
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(pass, salt);
+
+  const newUser = new User({
+    name: name,
+    lastname: lastname,
+    email: email,
+    username: username,
+    password: hashPass
+  });
+
+  newUser
+    .save()
+    .then(() => res.render("homepage"))
+    .catch(err => console.log(err));
+});
+
+/* GET home page */
+
+router.get("/homepage", (req, res, next) => {
+  res.render("homepage");
+});
 
 /* GET manage page */
 router.get("/manage", (req, res, next) => {
