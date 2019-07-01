c25be4cb2cbd03293e1c29e8b89850fcd8ade168
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-07-01 12:38:02 +0200

    add clothes models

---
+++ dressingProject/models/day-clothes.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dayClothesSchema = new Schema({
+  haut: {{clothes.id} required: true },
+  bas: id,
+  shoes: { type: String, required: true },
+  accessoires : 
+});
+
+const dayClothes = mongoose.model("dayClothes", dayClothesSchema);
+module.exports = dayClothes;
