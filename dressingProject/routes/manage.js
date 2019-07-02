5a398608423f5ea2ec5a22f923cf068108cbdca2
paulbourceret <paulbourceret@gmail.com>
2019-07-02 09:31:43 +0200

    debug

--- dressingProject/routes/manage.js
+++ dressingProject/routes/manage.js
@@ -2,51 +2,13 @@ const express = require("express");
 const router = new express.Router();
 const clothes = require("../models/clothes");
 const hbs = require("hbs");
-const types = require("../models/Types");
-
-router.get("/views", (req, res) => {
-  res.render("views");
-});
 
 router.get("/manage", (req, res) => {
   res.render("manage");
 });
 
-router.post();
-"/manage",
-  (req, res) => {
-    const {
-      name,
-      brand,
-      size,
-      type,
-      category,
-      price,
-      addeddate,
-      weareddate,
-      season,
-      image
-    } = req.body;
-  };
-
-console.log("ICII", req.body);
-if (
-  !name ||
-  !brand ||
-  !size ||
-  !type ||
-  !category ||
-  !price ||
-  !addeddate ||
-  weareddate ||
-  season
-);
-{
-  res.render("manage", { error: "Invalid input" });
-  return;
-}
-clothes
-  .create({
+router.post("/manage", (req, res) => {
+  const {
     name,
     brand,
     size,
@@ -55,10 +17,37 @@ clothes
     price,
     addeddate,
     weareddate,
-    season,
-    image
-  })
-  .then(() => res.redirect("/views"))
-  .catch(err => console.log(err));
+    season
+  } = req.body;
+  console.log("ICII", req.body);
+  // if (
+  //   !name ||
+  //   !brand ||
+  //   !size ||
+  //   !type ||
+  //   !category ||
+  //   !price ||
+  //   !addeddate ||
+  //   weareddate ||
+  //   season
+  // ) {
+  //   res.render("manage", { error: "Invalid input" });
+  //   return;
+  // }
+  clothes
+    .create({
+      name,
+      brand,
+      size,
+      type,
+      category,
+      price,
+      addeddate,
+      weareddate,
+      season
+    })
+    .then(() => res.redirect("/views"))
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
