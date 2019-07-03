4837b5e706c17ff1794cbd7c1dc7e4930336a7ac
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-07-03 18:07:09 +0200

    searchbar workin progress

--- dressingProject/public/javascripts/script.js
+++ dressingProject/public/javascripts/script.js
@@ -11,7 +11,10 @@ function getClothes() {
       .get("http://localhost:3000/clothes/api")
       .then(dbRes => {
         const myResult = dbRes.data.filter(oneElement => {
-          return oneElement.name.localeCompare(searchbar.value);
+          return oneElement.name
+            .toLowerCase()
+            .substring(0, searchbar.value.length)
+            .includes(searchbar.value.toLowerCase());
         });
         console.log(myResult);
       })
