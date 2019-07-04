bef470fb9054e3563a46a93e5a7bcb911e395247
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-07-04 11:40:26 +0200

    heroku fix

--- dressingProject/public/javascripts/script.js
+++ dressingProject/public/javascripts/script.js
@@ -1,25 +1,52 @@
 const searchbar = document.getElementById("myinput");
+const productGrid = document.querySelector(".products-grid");
+const meta = document.getElementById("site-url");
+const url = meta.getAttribute("data-url");
+console.log(url);
 
-// searchbar.oninput = () => {
-//   console.log(searchbar.value);
-//   console.log(getClothes());
-// };
+function createProductContent(arr) {
+  while (productGrid.firstChild) {
+    productGrid.removeChild(productGrid.firstChild);
+  }
+  arr.forEach(element => {
+    productGrid.insertAdjacentHTML(
+      "beforeend",
+      `<div class="containerClothes product-item-wrapper">
+    <a href="/viewone/${element._id}" class="product-item-wrapper">
+        <div class="product-img"><img src="${element.image}" alt="cloth"></div>
+        <p class="product-name">${element.name}</p>
+        <p class="product-name">${element.type}</p>
+        <p class="product-name">${element.brand}</p>
+        <p class="product-price">$${element.price}</p>
+    </a>
 
+    <button onclick="location.href='/delete/${element._id}'" type="button">
+        Delete Cloth</button>
+
+    <button onclick="location.href='/add/${element._id}'" type="button">
+        Add to the day</button>
+</div>`
+    );
+  });
+}
 function getClothes() {
   searchbar.oninput = () => {
     axios
-      .get("http://localhost:3000/clothes/api")
+      .get(`${url}/clothes/api`)
       .then(dbRes => {
         const myResult = dbRes.data.filter(oneElement => {
-          return oneElement.name
+          const dataToFilter = oneElement.name;
+          return dataToFilter
             .toLowerCase()
             .substring(0, searchbar.value.length)
             .includes(searchbar.value.toLowerCase());
         });
         console.log(myResult);
+        createProductContent(myResult);
       })
       .catch(dbErr => console.log(dbErr));
   };
 }
-
-getClothes();
+if (searchbar) {
+  getClothes();
+}
